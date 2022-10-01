dcf55382aa7f31f385ef94d11dd77b734f62856c
Chau Nguyen <nguyenngocchaugv@yahoo.com.vn>
2022-10-01 14:13:28 +0700

    store variable in env for server

--- server/index.js
+++ server/index.js
@@ -2,21 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRouters from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRouters);
-const CONNECTION_URL = 'mongodb+srv://social-media:12345678x%40X@cluster0.eivpxlk.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://social-media:12345678x%40X@cluster0.eivpxlk.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
